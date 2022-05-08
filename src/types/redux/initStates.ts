b782f6b1dad3bdc33870bfda412279b9a0a2c646
l1mb <sergeev.s1012@gmail.com>
2022-05-08 15:13:38 +0300

    ne zakomitilsya(

--- src/types/redux/initStates.ts
+++ src/types/redux/initStates.ts
@@ -1,19 +1,26 @@
+import { SexEnum } from "@/common/sex.enum";
+
 export type User = {
   email: string;
   firstName: string;
   lastName: string;
-  id: number;
+  id: string;
   roles: string[];
   profilePic: string;
   authorized: boolean;
+  birthDate: Date;
+  sex: SexEnum;
 };
 
 export const userInitState: User = {
   email: "",
   firstName: "",
   lastName: "",
-  id: 0,
+  id: "",
   roles: [],
   authorized: false,
-  profilePic: "",
+  sex: SexEnum.Male,
+  birthDate: new Date("2001-01-01"),
+  profilePic:
+    "https://res.cloudinary.com/dv1m78v3d/image/upload/c_thumb,w_200,g_face/v1650789446/ukrgenubujqt86buzrsk.jpg",
 };
