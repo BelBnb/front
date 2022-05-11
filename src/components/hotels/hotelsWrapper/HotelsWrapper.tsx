952fe92318a92982cc62c002af57c6f15a3634ed
Ivan S <ivan.skorodumov.21@gmail.com>
2022-05-11 09:40:16 +0300

    hotels-search-start

--- src/components/hotels/hotelsWrapper/HotelsWrapper.tsx
+++ src/components/hotels/hotelsWrapper/HotelsWrapper.tsx
@@ -10,12 +10,21 @@ const HotelsWrapper = () => {
     setTopLineClass(styles.endBorderLine);
   }, []);
 
+  const [name, setName] = useState("");
+  const [priceL, setPriceL] = useState(0);
+  const [priceB, setPriceB] = useState(10000);
+
   return (
     <div className={styles.hotelsWrapper}>
       <div className={styles.hotelsContainer}>
         <div className={`${styles.topLine} ${toplineClass}`}>
           <div className={styles.topItems}>
             <ColoredButton coloredLabel="Add hotel" onClick={() => {}} />
+            <div>
+              <input type={"text"} value={name} placeholder={"City name"} />
+              <input type={"text"} value={priceL} placeholder={"Price from"} />
+              <input type={"text"} value={priceB} placeholder={"Price to"} />
+            </div>
             <div className={styles.searchBarContainer}>
               <Searchbar />
             </div>
