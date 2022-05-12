3b2f5bd14846403818050e95ba8cedde00974b5e
l1mb <sergeev.s1012@gmail.com>
2022-05-12 22:59:39 +0300

    fix styles for neighbours

--- src/components/neighbours/neighbourComponent/NeighbourComponent.tsx
+++ src/components/neighbours/neighbourComponent/NeighbourComponent.tsx
@@ -1,6 +1,10 @@
 import { SexEnum } from "@/common/sex.enum";
 import { Neighbours } from "@/common/types/Neighbours";
+import ColoredButton from "@/elements/common/buttons/buttons";
+import { RootState } from "@/redux/store";
+import { User, userInitState } from "@/types/redux/initStates";
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./styles.module.scss";
 
 interface NeighbourComponentProps {
@@ -13,38 +17,38 @@ interface NeighbourComponentProps {
 }
 
 const NeighbourComponent: React.FC<NeighbourComponentProps> = ({ item, isMine, isDelete }) => {
+  const user = useSelector<RootState, User>((app) => app.user);
   console.log(item);
   return (
     <div className={styles.container}>
-      {item.userImage && (
-        <div className={styles.imageContainer}>
-          <img src={item.userImage} alt="альт" />
-        </div>
-      )}
+      <div className={styles.imageContainer}>
+        <img src={item.userImage || userInitState.profilePic || user.profilePic} alt="альт" />
+      </div>
       <div className={styles.flexDown}>
         <div className={styles.topLine}>
-          <div>
+          <div className={styles.topLineName}>
             <span>
               {item.userFirstName} {item.userLastName}
             </span>
-          </div>
-          <div className={styles.dates}>
-            <span>
-              Period: <span> {item.startDate} </span>
-            </span>
-            <span>
-              - <span> {item.endDate} </span>
-            </span>
+            <span>{item.sex === SexEnum.Female ? "Female" : "Male"}</span>
+            <span>{new Date().getFullYear() - new Date(item.birthDate).getFullYear()} years</span>
           </div>
         </div>
-        <span>{new Date().getFullYear() - new Date(item.birthDate).getFullYear()} years</span>
-        <span>{item.sex === SexEnum.Female ? "F" : "M"}</span>
         <span>{item.description}</span>
-        {isMine && isDelete && (
-          <button type="button" onClick={() => isDelete.onDelete(item.id)}>
-            Remove
-          </button>
-        )}{" "}
+      </div>
+      <div className={styles.rightColumn}>
+        <div className={styles.dates}>
+          <span>Time period:</span>
+          <span>
+            <span>From</span>
+            <span className={styles.date}>{item.startDate} </span>
+          </span>
+          <span>
+            <span>To</span>
+            <span className={styles.date}>{item.endDate} </span>
+          </span>
+        </div>
+        {isMine && isDelete && <ColoredButton coloredLabel="Remove" onClick={() => isDelete.onDelete(item.id)} />}{" "}
       </div>
     </div>
   );
