e03cd3809448968f6afbbfa5d09de55f7d45d380
l1mb <sergeev.s1012@gmail.com>
2022-05-12 23:07:09 +0300

    add onclick listener

--- src/components/neighbours/neighbourComponent/NeighbourComponent.tsx
+++ src/components/neighbours/neighbourComponent/NeighbourComponent.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { SexEnum } from "@/common/sex.enum";
 import { Neighbours } from "@/common/types/Neighbours";
 import ColoredButton from "@/elements/common/buttons/buttons";
@@ -5,6 +7,7 @@ import { RootState } from "@/redux/store";
 import { User, userInitState } from "@/types/redux/initStates";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 
 interface NeighbourComponentProps {
@@ -18,15 +21,20 @@ interface NeighbourComponentProps {
 
 const NeighbourComponent: React.FC<NeighbourComponentProps> = ({ item, isMine, isDelete }) => {
   const user = useSelector<RootState, User>((app) => app.user);
+  const navigate = useNavigate();
+
+  const goToProfile = (id: string) => {
+    navigate(`/profile/${id}`);
+  };
   console.log(item);
   return (
     <div className={styles.container}>
-      <div className={styles.imageContainer}>
+      <div className={styles.imageContainer} onClick={() => goToProfile(item.userId)}>
         <img src={item.userImage || userInitState.profilePic || user.profilePic} alt="альт" />
       </div>
       <div className={styles.flexDown}>
         <div className={styles.topLine}>
-          <div className={styles.topLineName}>
+          <div className={styles.topLineName} onClick={() => goToProfile(item.userId)}>
             <span>
               {item.userFirstName} {item.userLastName}
             </span>
